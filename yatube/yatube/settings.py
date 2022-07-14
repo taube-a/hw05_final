08f249491c6b52a592948f685b743f052d785156
taube-a <Taube.Anastasia@yandex.ru>
2022-07-14 08:22:16 +0300

    faet: add django-debug-toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
     'sorl.thumbnail',
 
     'about',
@@ -39,6 +40,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
